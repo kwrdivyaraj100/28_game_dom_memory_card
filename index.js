df6d4af8beebdadd433ad1020287a938e6fcd63f
Divya R <kwrdivyaraj100@gmail.com>
2023-10-13 15:41:26 +0530

    Picking random objects from the items array

--- index.js
+++ index.js
@@ -48,4 +48,22 @@ timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
 const movesCounter = () => {
 movesCount += 1;
 moves.innerHTML = `<span>Moves:</span>${movesCount}`;
+};    
+
+//Pick random objects from the items array
+const generateRandom = (size = 4) => {
+  //temporary array
+  let tempArray = [...items];
+  //initializes cardValues array
+  let cardValues = [];
+  //size should be double (4*4 matrix)/2 since pairs of objects would exist
+  size = (size * size) / 2;
+  //Random object selection
+  for (let i = 0; i < size; i++) {
+    const randomIndex = Math.floor(Math.random() * tempArray.length);
+    cardValues.push(tempArray[randomIndex]);
+    //once selected remove the object from temp array
+    tempArray.splice(randomIndex, 1);
+  }
+  return cardValues;
 };
