be29653575df881df4fec36f19919521ec09cfa4
Divya R <kwrdivyaraj100@gmail.com>
2023-10-14 12:31:16 +0530

    Logic for flipping cards

--- script.js
+++ script.js
@@ -88,3 +88,29 @@ const flipBackCards = () => {
 
     state.flippedCards = 0
 }
+
+const flipCard = card => {
+    state.flippedCards++
+    state.totalFlips++
+
+    if (!state.gameStarted) {
+        startGame()
+    }
+
+    if (state.flippedCards <= 2) {
+        card.classList.add('flipped')
+    }
+
+    if (state.flippedCards === 2) {
+        const flippedCards = document.querySelectorAll('.flipped:not(.matched)')
+
+        if (flippedCards[0].innerText === flippedCards[1].innerText) {
+            flippedCards[0].classList.add('matched')
+            flippedCards[1].classList.add('matched')
+        }
+
+        setTimeout(() => {
+            flipBackCards()
+        }, 1000)
+    }
+}
